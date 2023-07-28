5b0fa2a2dccb0a354e93794807f86dd5defb660c
liquibread <buhati.d@gmail.com>
2023-07-28 10:24:04 +0300

    idk

--- passgen.py
+++ passgen.py
@@ -2,76 +2,54 @@ from tkinter import *
 from tkinter import ttk
 import secrets
 import string
-import pyperclip
 
-'''
+### function defs
 
-#define main window
-main = Tk() 
-main.title("super strong password generator <3")
-main.geometry("500x100+200+200")
-
-test = ttk.Label(text="aaaaaaaaaaaaa")
-test.pack(side = ttk.left)
-
-#function to make text copy-able
-def focusText(event):
-    w.config(state='normal')
-    w.focus()
-    w.config(state='disabled')
-
-w = Text(main, height=1, borderwidth=0, )
-w.insert(1.0, "Hello, world!")
-w.pack()
-
-
-'''
-
-
-pwd = ''
-def callback():
-    while True:
-        pwd = ' '
-        for i in range(pwd_length):
-            pwd += ' '.join(secrets.choice(alphabet))
-        if (sum(char in special for char in pwd)<=2 and
-            sum(char in digits for char in pwd)>=2 and
-            sum(char in digits for char in pwd)<=5 and
-            sum(char in letters for char in pwd)>=5):
-            break
-    print(pwd)
-
-root = Tk()
-root.geometry('500x350')
-root.title("super strong password generator <3")
-frm = ttk.Frame(root, padding=155, width=0, height=500)
-frm.grid()
-
-
-
-ttk.Label(frm, text="Secure password generator.").grid(column=0, row=0)
-ttk.Button(frm, text="Generate!", command=callback).grid(column=0, row=1)
-
-
-root = Tk()
-frm1 = ttk.Frame(root, padding=100, width=1000, height=500)
-frm1.grid()
-ttk.Label(frm1, text="Your new password is:").grid(column=0, row=0)
-ttk.Label(frm1, text=pwd).grid(column=0, row=1)
-ttk.Button(frm1, text="OK", command=root.quit).grid(column=0, row=2)
-
-
-
+### important variables
 letters = string.ascii_letters
 digits = string.digits
 special = string.punctuation
 
 alphabet = letters + digits + special
 
-pwd_length = 13
+pwd_length = 12
 
+def callback():
+    while True:
+        pwd = ''
+        for i in range(pwd_length):
+            pwd += ''.join(secrets.choice(alphabet))
+            ### checks to see if there are (at least) 2 special characters, 
+            ### between 2 and 5 digits, more than 5 letters
+        if (sum(char in special for char in pwd)<=2 and
+            sum(char in digits for char in pwd)>=2 and
+            sum(char in digits for char in pwd)<=5 and
+            sum(char in letters for char in pwd)>=5):
+            break
+    #frm1 = ttk.Frame(main, padding=100, width=1000, height=500)
+    #frm1.grid()
+    
+    ### copies the password to clipboard
+    ### doesnt work (
+    def copy(text):
+        main.clipboard_clear()
+        main.clipboard_append(text)
+    ### add the generated pass to the window and add a 'copy' button
+    ttk.Label(frm, text="Your new password is:").grid(column=0, row=2)
+    ttk.Label(frm, text=pwd).grid(column=0, row=3)
+    ttk.Button(frm, text="copy", command=copy(pwd)).grid(column=0, row=4)
 
+### end function defs
 
-root.mainloop()
+### characteristics for the main window
+main = Tk()
+main.geometry('500x350')
+main.title("super strong password generator <3")
+frm = ttk.Frame(main, padding=150, width=0)
+frm.grid()
 
+#main = Tk()
+ttk.Label(frm, text="Secure password generator.").grid(column=0, row=0)
+ttk.Button(frm, text="Generate!", command=callback).grid(column=0, row=1)
 
+main.mainloop()
